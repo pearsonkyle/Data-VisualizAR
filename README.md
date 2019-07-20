a9e366d70bd203cabe84c163dae5794eae5cde05
Kyle Pearson <pearsonk@email.arizona.edu>
2019-07-19 22:48:27 -0700

    dynamic model webpage

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Data-VisualizAR
 Create and visualize 3D data and models (.obj) in augmented reality through the web with AR.js
+
+- :iphone: :phone: Open this link on your phone 
+- Point camera at picture below 
+
+![](static/patterns/pattern-earth.png)
+
+## Example 
--- web_ar.py
+++ web_ar.py
@@ -1,6 +1,27 @@
 from flask import Flask, current_app
 app = Flask(__name__)
 
+@app.route('/models/<page>')
+def load_model(page):
+    htmlpage = '''
+    <!doctype HTML>
+    <html>
+    <script src="https://aframe.io/releases/0.9.1/aframe.min.js"></script>
+    <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.7.2/aframe/build/aframe-ar.js"></script>
+
+    <body style='margin : 0px; overflow: hidden;'>
+        <a-scene embedded arjs>
+            <a-marker id="memarker" type="pattern" url="../static/patterns/pattern-earth.patt" vidhandler>
+                <a-entity obj-model="obj: url(../static/models/{}.obj); mtl: url(../static/models/{}.mtl)" scale="0.1 0.1 0.1"> </a-entity>
+            </a-marker>
+            <a-entity camera></a-entity>
+        </a-scene>
+    </body>
+
+    </html>
+    '''.format(page,page)
+    return htmlpage
+
 @app.route('/<page>')
 def load_page(page):
     return current_app.send_static_file('{}.html'.format(page))
@@ -11,6 +32,3 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True,port=4000,ssl_context='adhoc')
-    # create script to animate 3D models 
-    # blender folder? 
-    # static examples 
